cec404ea12059906953784d5ebd994b21d497727
Jessica Webster <jessicawebster@weber.edu>
2021-10-18 15:10:36 -0600

    Attempting to add empty project

---
+++ app/src/main/java/CS3750/Fall21/MainActivity.java
@@ -0,0 +1,14 @@
+package CS3750.Fall21;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
