534a4d6b527b6b8a99a18a55823f4cd0bcbc4161
Jessica Webster <jessicawebster@weber.edu>
2021-10-19 11:48:48 -0600

    Added Button and working on doing something with button

--- app/src/main/java/CS3750/Fall21/MainActivity.java
+++ app/src/main/java/CS3750/Fall21/MainActivity.java
@@ -4,11 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+ArrayList<String> OrderList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        android.widget.Button btn_order_1 = (android.widget.Button) findViewById(R.id.order_1);
+        btn_order_1.setOnClickListener (new android.view.View.OnClickListener() {
+            public void onClick(android.view.View v) {
+                //Do something in response to button click
+                OrderList.add(btn_order_1.getText().toString());
+            }
+        });
     }
 }
+
+/* --Create Activity to start after button click
+button.setOnClickListener(new OnClickListener() {
+@Override
+public void onClick(View v) {
+    Intent intent = new Intent(this, YourActivityName.class);
+    startActivity(intent);
+}
+});
+ */
